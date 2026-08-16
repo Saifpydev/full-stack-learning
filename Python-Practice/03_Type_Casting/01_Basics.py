cdf1959c8d951a00b2dbe16a72bf216b6b425f9c
Saif <saif.faridi20@gmail.com>
2026-08-16 16:23:35 +0530

    Add Python Pratice

---
+++ Python-Practice/03_Type_Casting/01_Basics.py
@@ -0,0 +1,64 @@
+
+# String --> Integer
+
+age = 25 
+age = int(age)
+print(age)
+print(type(age))
+
+# String --> Float
+
+price = "99.50"
+prince = float(price)
+
+print(price)
+print(type(price))
+
+# Integer --> Float 
+
+number = 25
+
+number = float(number)
+print(number)
+print(type(age))
+
+# Float --> Intger
+
+price= 99.64
+
+price = int(price)
+
+print(price)
+print(type(price))
+
+# String --> Boolean
+
+value = "Python"
+
+value = bool(value)
+
+print(value)
+print(type(value))
+
+# Integer --> Boolean
+
+number = 0
+print(bool(number))
+number = 10
+print(bool(number))
+
+
+# Boolean --> Integer
+
+a = True
+b = False
+
+print(int(a))
+print(int(b))
+
+# Boolean -> String
+
+status = True
+status = str(status)
+print(status)
+print(type(status))
