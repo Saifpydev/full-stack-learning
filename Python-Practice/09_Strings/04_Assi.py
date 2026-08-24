13253fd978d6ae9d4c004a7a62bd6603a5b18429
Saif <saif.faridi20@gmail.com>
2026-08-24 15:29:40 +0530

    New solve question

---
+++ Python-Practice/09_Strings/04_Assi.py
@@ -0,0 +1,11 @@
+# Take a string from the user and print
+# Length Uppercase lowercase Firstcharacter lastcharacter
+
+text = "I am a fullstack developer my strong language is python"
+print(len(text))
+print(text.upper())
+print(text.lower())
+print(text[0])
+print(text[-1])
+
+
