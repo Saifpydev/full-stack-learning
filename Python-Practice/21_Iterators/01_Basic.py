5641ad3edd95defbf51e50f5ab5a452faac961cd
Saif <saif.faridi20@gmail.com>
2026-09-04 16:13:04 +0530

    New update

---
+++ Python-Practice/21_Iterators/01_Basic.py
@@ -0,0 +1,37 @@
+numbers = [10, 20, 30 ,80, 40, 90, 50]
+it = iter(numbers)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+
+
+
+# Create a tuple: 
+# convert it into an iterator and use next() to print each name.
+
+names = ("saif", "Rahul", "Aman")
+it = iter(names)
+print(next(it))
+print(next(it))
+print(next(it))
+
+
+# Create a string Convert it into an iterator and print its characters one by one using next()
+
+word  = "Pyhton"
+
+
+
+
+# create a list Use an itertor and next() to print the values Then try one extra next() and observe StopitERATION
+
+
+
+
+
+# Create a custom iterator that iterates through a list in reverse order
+
