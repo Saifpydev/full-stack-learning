45d1625aec67aa568e136960db0f954234aceb5d
Saif <saif.faridi20@gmail.com>
2026-09-04 16:22:11 +0530

    New Update

--- Python-Practice/21_Iterators/01_Basic.py
+++ Python-Practice/21_Iterators/01_Basic.py
@@ -24,14 +24,50 @@ print(next(it))
 
 word  = "Pyhton"
 
+it = iter(word)
 
-
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
 
 # create a list Use an itertor and next() to print the values Then try one extra next() and observe StopitERATION
 
+numbers = [10, 20, 30]
+it = iter(numbers)
 
-
-
+print(next(it))
+print(next(it))
+print(next(it))
 
 # Create a custom iterator that iterates through a list in reverse order
 
+class ReverseIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data) - 1
+        
+    def __int__(self):
+        return self
+    
+    def __next__(self):
+        
+        if self.index < 0:
+            raise StopIteration
+        
+        value = self.data[self.index]
+        self.index -= 1
+        
+        return value
+    
+numbers = [10, 20, 30, 40, 60, 50]
+    
+it = ReverseIterator(numbers)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
