c39b52166d42a23628ee97aa7239f7eac75a41bd
Saif <saif.faridi20@gmail.com>
2026-09-05 16:27:57 +0530

    New update

---
+++ Python-Practice/24_Regular_Expressions/01_Basics.py
@@ -0,0 +1,33 @@
+# Given text = "My phone number is 9858965620" regex se 10-digit phone number extract karo.
+
+import re
+
+text = "My phone number is 9878986652"
+
+result = re.search(r"\d{10}",text)
+
+print(result.group())
+
+# Given text = "Contact us at test@gmail.com or hello@yahoo.com regex ka use karke saare email address extract karo",..
+
+import re
+
+text = "Contact us at test@gmail.com or hello@yahoo.com"
+
+emails = re.findall(r"[\w.-]+@[\w.-]+\.\w+", text)
+
+print(emails)
+
+
+
+# Given text = "Python 100, java 200, React 300" Regex se saare numbers extract karo
+# ['100', "200" "3000"]
+
+
+
+# Check karo ki user ka password valid hai ya nhi
+# Requirements : Minimum 8 characters
+# At least 1 uppercase letter
+# At least 1 lowercase letter
+# At least 1 digit
+
