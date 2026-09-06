068035f16352850c05ba326713e632979d8ada0d
Saif <saif.faridi20@gmail.com>
2026-09-06 13:52:10 +0530

    New update

--- Python-Practice/25_Date_Time/01_Import.py
+++ Python-Practice/25_Date_Time/01_Import.py
@@ -29,3 +29,28 @@ print(now.strftime("%d-%m-%Y"))
 date_string = "06-09-2026"
 date_object = datetime.strptime(date_string, "%d-%m-%Y")
 print(date_object)
+
+
+
+# Calculate Age
+# Write a Python program to calculate a person's age from theri birth year.
+
+from datetime import date
+birth_year = int(input("Enter your birth year: "))
+current_year = date.today().year
+age = current_year - birth_year
+print("Your age is:", age)
+
+
+
+# find the Date After 30 Days
+# write a Python program to find the date after 30 days from today
+
+from datetime import date, timedelta
+today = date.today()
+
+after_30_days = today + timedelta(days=30)
+
+print("Today's date:", today)
+print("Date after 30 days:", after_30_days)
+
