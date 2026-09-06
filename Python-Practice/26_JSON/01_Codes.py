6157d7f388f24ce323c804122ad0f2bd7cf46892
Saif <saif.faridi20@gmail.com>
2026-09-06 15:15:50 +0530

    New update

--- Python-Practice/26_JSON/01_Codes.py
+++ Python-Practice/26_JSON/01_Codes.py
@@ -23,4 +23,31 @@ json_data = '{"name": "Saif", "age": 25}'
 python_data = json.loads(json_data)
 print(python_data)
 
+
+# Read Data from a JSON File
+# Read the data from studnet.json and print the student's name and course.
+
+import json
+with open("26_JSON/02_Student.json", "r") as file:
+    student = json.load(file)
+    
+print("Name:", student["name"])
+print("Course:", student["course"])   
+
+student["age"]  = 24
+with open("26_JSON/02_Student.json", "w") as file:
+    json.dump(student, file, indent =4)
+    
+print("Age updated successfully!")    
+    
+    
+
+
+
+
+
+# Update JSON Date
+# Read student.json chnage the student's age from 23 to 24, and save the updated data back to the file..
+
+
  
